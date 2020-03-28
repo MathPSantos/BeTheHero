43571239b85dacee8cd26413bfe1713360351f6b
Matheus Santos <matheus.psantos2016@gmail.com>
2020-03-28 19:30:00 -0300

    Profile page translated

--- frontend/src/pages/Proile/index.js
+++ frontend/src/pages/Proile/index.js
@@ -49,28 +49,28 @@ export default function Profile() {
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
-                <span>Bem vinda, {ongName}</span>
+                <span>Welcome, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">
-                    Cadastrar novo caso
+                    Add new incident
                 </Link>
                 <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
-            <h1>Casos Cadastrados</h1>
+            <h1>Current Incidents</h1>
 
             <ul>
                 {incidents.map(incident => (
                     <li key={incident.id}>
-                        <strong>CASO:</strong>
+                        <strong>INCIDENT:</strong>
                         <p>{incident.title}</p>
 
-                        <strong>DESCRIÇÃO:</strong>
+                        <strong>DESCRIPTION:</strong>
                         <p>{incident.description}</p>
 
-                        <strong>VALOR:</strong>
-                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+                        <strong>VALUE:</strong>
+                        <p>{Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(incident.value)}</p>
 
                         <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                             <FiTrash2 size={20} color="a8a8b3" />
